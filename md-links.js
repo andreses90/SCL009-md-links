699527aef283f6f10e41310726910f628720c736
Giselle Machado <gisellemachadoyes@gmail.com>
2019-07-07 20:07:22 -0400

    historia 8 concluida - funcionalidad stats+validate

--- md-links.js
+++ md-links.js
@@ -157,6 +157,17 @@ const mdLinks = (path, options) => {
       .catch(err => {
         reject(err);
       });
+    } else if (options[0] === "--stats" && options[1] === "--validate") {
+      isFileOrDirectory(path)
+      .then(links => {
+        validate(links)
+        .then(res => {
+          stats(res)
+          .then(res => {
+            resolve(res);
+          });
+        });
+      });  
     } else if (options[0] === "--validate" && options[1] === undefined) {
       isFileOrDirectory(path)
       .then(links => {
